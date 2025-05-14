e403c82ec14f89a78302d2daea71e3e2ddbee151
Harry Chambers <hc332@student.le.ac.uk>
2025-05-14 23:48:57 +0100

    Added encoding to preprocess file

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from dataloader import WineDataHandler
+from preprocess import preprocess
 import pandas as pd
+from IPython.display import display
 
 # Define the URLs
 red_wine_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
@@ -15,4 +17,6 @@ combined_wine_df = wine_loader.combined_df
 #wine_loader.plot_scatter_quality()
 # Encode wine type and get the encoded DataFrame
 
-combined_wine_df = wine_loader.encode_wine_type()
+combined_wine_df = preprocess.encode_wine_type(combined_wine_df)
+
+display(combined_wine_df)
